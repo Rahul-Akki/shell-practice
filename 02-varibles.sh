5dc5abd2ce5d2f46600bc4ca4e69afa91350a486
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-28 11:31:30 -0500

    roboshop shell script

--- 02-varibles.sh
+++ 02-varibles.sh
@@ -5,4 +5,4 @@ P2=lakshman
 
 echo "$P1 : Hello $P2, How is it going ?"
 echo "$P2 : I Am doing pretty good $P1, how about you ?"
-echo "$P1 : I Am doing good $P2, Thanks for asking"
+echo "$P1 : I Am doing good $P2, Thanks for asking."
---
+++ 03-varibles-run-command.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DATE=$(date '+%A %W %Y %X')
+echo "script started executing at : $DATE"
