7efc7d66614e8d00f1d6c578426377eb634c0ca4
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-28 12:27:08 -0500

    roboshop shell script

--- 08.conditions-if.sh
+++ 08.conditions-if.sh
@@ -3,7 +3,7 @@ CAR=$1
 
 if [ $CAR -lt 60000 ]
 then
-    echo "Car price $CAR is in my Budget to buy"
+    echo "Car price $$CAR is in my Budget to buy"
 else
-    echo "Car price $CAR is not in my Budget"
+    echo "Car price $$CAR is not in my Budget"
 fi
