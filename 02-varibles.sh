3e7fb8e2d47e3c8956f2981bd7097423aca423ac
Rahul-Akki <rahul.akireddy@gmail.com>
2024-02-28 11:26:54 -0500

    roboshop shell script

---
+++ 02-varibles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+P1=ram
+P2=lakshman
+
+echo "$P1 : Hello $P2, How is it going ?"
+echo "$P2 : I Am doing pretty good $P1, how about you ?"
+echo "$P1 : I Am doing good $P2, Thanks for asking"
