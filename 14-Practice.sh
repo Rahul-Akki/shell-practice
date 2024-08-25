1687c8db298d0a2a33294ac0ac5a692d3b3f3507
Rahul-Akki <rahul.akireddy@gmail.com>
2024-08-25 17:51:56 -0400

    Edited

--- 14-Practice.sh
+++ 14-Practice.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-NUMBER=$1
+#Algorithm to install mysql
+#1 --> Check the sudo access --> 
+#1.1 --> if we do not have sudo asses --> exit --> id -u -ne 0 
+#1.2 --> if we have sudo asses --> Move to next step
+#2 --> check if already installed --> < yum list installed mysql >
+#2.1 --> if already installed --> exit
+#2.1 --> if not installed --> Move to next step
+#3 --> install my sql --> < yum install mysql -y >
+#3.1 --> if installed Sucessfully --> Print installed sucessfully
+#3.1 --> if not installed Sucessfully --> Print Error
 
-if [ $NUMBER -gt 100 ]
+ID=$(id -u)
+
+if [ $ID -ne 0  ]
 then
-    echo "given num $NUMBER is great"
+    echo "ERROR :: Please run with root access"
 else
-    echo "given num $NUMBER iS LESS"
+    echo " You are root user "
 fi
